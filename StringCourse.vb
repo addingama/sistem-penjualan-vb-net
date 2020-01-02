09618a0be119736993d5cd33bd929b5b61c84373
Addin Gama Bertaqwa <punyagama@gmail.com>
2020-01-02 10:32:13 +0800

    upload the rest of the files

---
+++ StringCourse.vb
@@ -0,0 +1,20 @@
+﻿
+Option Explicit On
+Option Strict On
+
+Imports System
+
+
+
+Public Class StringCourse
+	
+	Public Sub New()
+		MyBase.New
+	End Sub
+    
+    Public Sub StringDefinition()
+        
+            System.Console.WriteLine("String cuy")
+        
+    End Sub
+End Class
