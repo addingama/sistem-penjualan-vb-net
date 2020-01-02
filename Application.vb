3a6b33cdb2280b37c5aea540ed84f0d68fb39005
Addin Gama Bertaqwa <punyagama@gmail.com>
2020-01-02 10:11:00 +0800

    - tambahkan .gitignore - add sample vb file

---
+++ Application.vb
@@ -0,0 +1,17 @@
+﻿
+
+Public Class Application
+	 Sub Main()
+		DoLoop()
+	End Sub
+    
+    
+    Sub DoLoop()
+        Dim a As Integer = 10
+        Do 
+            System.Console.WriteLine(a)
+            a = a + 1
+            
+        Loop While (a < 20)
+    End Sub
+End Class
